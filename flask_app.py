6d55047c5cccbb13dfd7f6b1692ac9809ba77063
DevrathMukesh <95998192+DevrathMukesh@users.noreply.github.com>
2024-04-03 15:58:19 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,6 @@ from utils import fetch_conversion_factor
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['POST'])
 def index():
     data = request.get_json()
@@ -11,21 +10,15 @@ def index():
     amount = float(data['queryResult']['parameters']['unit-currency']['amount'])
     target_currency = data['queryResult']['parameters']['currency-name']
     print(source_currency)
-    print(target_currency)
-    print(amount)
-
-    response_text = ""
     cf = fetch_conversion_factor(source_currency, target_currency)
-    print(cf)
     if cf is not None:
         final_amount = amount * cf
-        response_text += "{} {} is {} {}\n".format(amount, source_currency, final_amount, target_currency)
+        response_text = f"{amount} {source_currency} is {final_amount} {target_currency}"
     else:
-        response_text += f"Error: Unable to fetch conversion factor for {source_currency} to {target_currency}\n"
+        response_text = f"Error: Unable to fetch conversion factor for {source_currency} to {target_currency}"
 
     response = {'fulfillmentText': response_text}
     return jsonify(response)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
